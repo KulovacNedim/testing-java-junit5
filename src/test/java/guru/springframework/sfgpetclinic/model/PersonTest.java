4c8b82d2c320cb87715a432957585c9ca34d1710
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-25 09:31:40 +0100

    Grouped assesrtions demonstrated.

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        Person person = new Person(1L, "Joe", "Buck");
+
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Joe"),
+                () -> assertEquals(person.getLastName(), "Buck"));
+    }
+
+    @Test
+    void groupedAssertionsMsg() {
+        Person person = new Person(1L, "Joe", "Buck");
+
+        assertAll("Test Props Set",
+                // broke properties to see failing messages
+                () -> assertEquals(person.getFirstName(), "Joe", "First Name Failed"),
+                () -> assertEquals(person.getLastName(), "Buck", "Last Name Failed"));
+    }
+}
